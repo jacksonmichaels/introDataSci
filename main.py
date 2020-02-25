8c1013de6a2a63b3850f34acd1ed4c1bf0b50af5
Jackson Michaels <jackson.michaels@mymail.champlain.edu>
2020-02-25 13:10:38 -0500

    added multiple calls per thread

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ BASE_URL = 'https://api.rawg.io/api/games'
 
 important_cols = ['id', 'slug', 'name', 'released', 'rating', 'ratings_count', 'metacritic', 'playtime']
 genres = []
-num_pages = 1000
+parent_platforms = []
+num_threads = 120
+num_pages_per_thread = 150
 
-thicc_results = [[] for i in range(num_pages)]
+
+thicc_results = [[] for i in range(num_threads * num_pages_per_thread)]
 
 def get_genres():
     genres = requests.get('https://api.rawg.io/api/genres')
@@ -18,27 +21,41 @@ def get_genres():
     shorter.sort(key=lambda x:x[0])
     return shorter
 
+def get_parent_consoles():
+    genres = requests.get('https://api.rawg.io/api/platforms/lists/parents')
+    data = json.loads(genres.text)['results']
+    shorter = [(x['id'], x['name']) for x in data]
+    shorter.sort(key=lambda x:x[0])
+    return shorter
 
-def getGames(page, arr):
-    if page != 0:
-        games = requests.get(BASE_URL + "?page=" + str(page))
-    else:
-        games = requests.get(BASE_URL)
-    game_data = json.loads(games.text)
-    results = game_data['results']
-    arr[page] = results
-    print(page)
-    return results
+# end code (jackson)
+# start code (connor)
+def getGames(pages, arr):
+    for page in pages:
+        if page != 0:
+            games = requests.get(BASE_URL + "?page=" + str(page))
+        else:
+            games = requests.get(BASE_URL)
+        game_data = json.loads(games.text)
+        results = game_data['results']
+        arr[page] = results
+    print(pages[0], " - ", pages[-1])
+# end code (connor)
+# start code (jackson)
 
 def convertRow(row):
-    newRow =[row[x] for x in important_cols]
+    newRow = [row[x] for x in important_cols]
     genreNums = [x['id'] for x in row['genres']]
+    parentNums = [x['platform']['id'] for x in row['parent_platforms']]
     genreRow = [int(x[0]) in genreNums for x in genres]
+    platformRow = [int(x[0]) in parentNums for x in parent_platforms]
     newRow += genreRow
+    newRow += platformRow
     return newRow
 
+
 def startGameThreads():
-    threads = [Thread(target=getGames, args=(i,thicc_results)) for i in range(num_pages)]
+    threads = [Thread(target=getGames, args=([j + i * num_pages_per_thread for j in range(num_pages_per_thread)], thicc_results)) for i in range(num_threads)]
     print("threads made")
     for thread in threads:
         thread.start()
@@ -48,22 +65,24 @@ def startGameThreads():
         thread.join()
 
     print("threads done")
-    print(thicc_results)
 
     flat_list = [item for sublist in thicc_results for item in sublist]
 
 
     converted_list = [convertRow(y) for y in flat_list]
 
-    col_names = important_cols + [x[1] for x in genres]
+    col_names = important_cols + [x[1] for x in genres] + [x[1] for x in parent_platforms]
 
     df = pd.DataFrame(converted_list, columns=col_names)
 
     return df
 
 genres = get_genres()
+parent_platforms = get_parent_consoles();
 df = startGameThreads()
 
 df.to_csv("data/butt.csv")
+
+print(df.head())
 
 print("real done")
